216ce2bece7921c305e847541e752d0df898835a
Robin Roschlau <robin@roschlau.me>
2024-06-24 11:39:12 +0200

    Add tauri wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,17 @@ import App from './App.tsx'
 import {Toaster} from 'react-hot-toast'
 import {Tooltip} from 'react-tooltip'
 import './index.css'
+import {invoke} from '@tauri-apps/api'
+
+invoke('greet', {name: 'Brickfilmers'})
+  .then(
+    (result) => console.log(result),
+    (error) => console.error(error),
+  )
 
 const root = document.getElementById('root')
 if (!root) {
-    throw Error('Root element not found')
+  throw Error('Root element not found')
 }
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
